dcba54052edb8400680167487049e425ac49d496
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2024-02-11 18:11:12 +0530

    Revert "multiple teachers access"
    This reverts commit 1ffe6494e9eed784d835f6c9fb61759bd926730b.

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,5 +1,3 @@
 export const isTeacher = (userId?: string | null) => {
-  const teacherIdsStr = process.env.NEXT_PUBLIC_TEACHER_ID || "[]"; 
-  const teacherIds = JSON.parse(teacherIdsStr);
-  return Array.isArray(teacherIds) && teacherIds.includes(userId);
-};
+  return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
+}
