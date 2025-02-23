2edc797421c5a5a8dbf07c7994f948e916df5f2a
Yaswanth B <b.v.s.yaswanth1@gmail.com>
2025-02-23 14:18:56 +0530

    anonymise
    Signed-off-by: Yaswanth B <b.v.s.yaswanth1@gmail.com>

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -4,16 +4,18 @@ export const Logo = () => {
   return (
     <>
       <div className="flex items-center">
-        <Image src={"/jntugv.jpeg"} alt="Hero image" width={50} height={50} />
-        <h1 className="text-violet-600 font-bold mr-4 ml-4 align-right">
-          JNTU-GV
-        </h1>
+        <Image src={"/cg.jpg"} alt="Hero image" width={450} height={50} />
       </div>
       <div className="flex items-center">
+        <h1 className="text-blue-600 font-bold mr-4 ml-4 align-right pl-12 pt-10">
+          BanKING
+        </h1>
+      </div>
+      {/* <div className="flex items-center">
         <h1 className="text-blue-600 font-bold mr-4 ml-4 align-right pl-12">
           LMS
         </h1>
-      </div>
+      </div> */}
     </>
   );
 };
